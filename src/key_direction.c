c704bbbabb31ce67eb11566db3ef4bf9ff7e9d01
junhyupa <junhyupa@studenr.42seoul.kr>
2023-05-26 15:49:50 +0900

    key_direction.c norm checked

--- src/key_direction.c
+++ src/key_direction.c
@@ -6,47 +6,48 @@
 /*   By: junhyupa <junhyupa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 15:30:56 by junhyupa          #+#    #+#             */
-/*   Updated: 2023/05/26 15:33:09 by junhyupa         ###   ########.fr       */
+/*   Updated: 2023/05/26 15:48:42 by junhyupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../inc/cub3d.h"
+#include "../inc/cub3d.h"
 
 void	keypress_w(t_data *data)
 {
-	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x + (data->player.dir_x * (data->player.move_speed + 0.15)))] != '1')
+	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x \
+	+ (data->player.dir_x * (data->player.move_speed + 0.15)))] != '1')
 		data->player.pos_x += data->player.dir_x * data->player.move_speed;
-	printf("%c\n",data->cub.map[(int)(data->player.pos_y + (data->player.dir_y * data->player.move_speed + 0.15))][(int)(data->player.pos_x)]);
-	if (data->cub.map[(int)(data->player.pos_y + (data->player.dir_y * (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
+	if (data->cub.map[(int)(data->player.pos_y + (data->player.dir_y \
+	* (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
 		data->player.pos_y += data->player.dir_y * data->player.move_speed;
-	printf("x : %f y:%f\n", data->player.pos_x, data->player.pos_y);
 }
 
 void	keypress_a(t_data *data)
 {		
-	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x - (data->player.plane_x * (data->player.move_speed + 0.15)))] != '1')
+	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x \
+	- (data->player.plane_x * (data->player.move_speed + 0.15)))] != '1')
 		data->player.pos_x -= data->player.plane_x * data->player.move_speed;
-	printf("%c\n",data->cub.map[(int)(data->player.pos_y + (data->player.dir_y * data->player.move_speed + 0.15))][(int)(data->player.pos_x)]);
-	if (data->cub.map[(int)(data->player.pos_y - (data->player.plane_y * (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
+	if (data->cub.map[(int)(data->player.pos_y - (data->player.plane_y \
+	* (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
 		data->player.pos_y -= data->player.plane_y * data->player.move_speed;
-	printf("x : %f y:%f\n", data->player.pos_x, data->player.pos_y);
 }
 
 void	keypress_s(t_data *data)
 {
-	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x - (data->player.dir_x * (data->player.move_speed + 0.15)))] != '1')
+	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x \
+	- (data->player.dir_x * (data->player.move_speed + 0.15)))] != '1')
 		data->player.pos_x -= data->player.dir_x * data->player.move_speed;
-	if (data->cub.map[(int)(data->player.pos_y - (data->player.dir_y * (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
+	if (data->cub.map[(int)(data->player.pos_y - (data->player.dir_y \
+	* (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
 		data->player.pos_y -= data->player.dir_y * data->player.move_speed;
-	printf("x : %f y:%f\n", data->player.pos_x, data->player.pos_y);	
 }
 
 void	keypress_d(t_data *data)
 {
-	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x + (data->player.plane_x * (data->player.move_speed + 0.15)))] != '1')
+	if (data->cub.map[(int)(data->player.pos_y)][(int)(data->player.pos_x \
+	+ (data->player.plane_x * (data->player.move_speed + 0.15)))] != '1')
 		data->player.pos_x += data->player.plane_x * data->player.move_speed;
-	printf("%c\n",data->cub.map[(int)(data->player.pos_y + (data->player.dir_y * data->player.move_speed + 0.15))][(int)(data->player.pos_x)]);
-	if (data->cub.map[(int)(data->player.pos_y + (data->player.plane_y * (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
+	if (data->cub.map[(int)(data->player.pos_y + (data->player.plane_y \
+	* (data->player.move_speed + 0.15)))][(int)(data->player.pos_x)] != '1')
 		data->player.pos_y += data->player.plane_y * data->player.move_speed;
-	printf("x : %f y:%f\n", data->player.pos_x, data->player.pos_y);
 }
--- src/loop.c
+++ src/loop.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   loop.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hunpark <hunpark@student.42.fr>            +#+  +:+       +#+        */
+/*   By: junhyupa <junhyupa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 03:08:15 by hupa              #+#    #+#             */
-/*   Updated: 2023/05/26 15:12:05 by hunpark          ###   ########.fr       */
+/*   Updated: 2023/05/26 15:49:11 by junhyupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	loop(t_data *data)
 {
 	mlx_loop_hook(data->mlx.mlx_ptr, &main_loop, data);
 	mlx_hook(data->mlx.mlx_win, X_EVENT_KEY_PRESS, 1L << 0, &key_press, data);
-	mlx_hook(data->mlx.mlx_win, X_EVENT_KEY_EXIT, 1L << 0, &key_exit, data);
+	mlx_hook(data->mlx.mlx_win, X_EVENT_KEY_EXIT, 1L << 0, &keypress_exit, data);
 	mlx_loop(data->mlx.mlx_ptr);
 }
