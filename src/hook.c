d3492d03a9f1933d2635dd3da7a36f7cf9ebd695
junhyupa <junhyupa@studenr.42seoul.kr>
2023-05-26 15:53:23 +0900

    hook.c norm checked

--- src/hook.c
+++ src/hook.c
@@ -6,7 +6,7 @@
 /*   By: junhyupa <junhyupa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 21:27:39 by hupa              #+#    #+#             */
-/*   Updated: 2023/05/26 15:40:50 by junhyupa         ###   ########.fr       */
+/*   Updated: 2023/05/26 15:52:15 by junhyupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,36 @@ int	keypress_exit(t_data *data)
 
 void	keypress_right(t_data *data)
 {
-	double olddir_x = data->player.dir_x;
-	double oldplane_x = data->player.plane_x;
-	data->player.dir_x = data->player.dir_x * cos(data->player.rot_speed) - data->player.dir_y * sin(data->player.rot_speed);
-	data->player.dir_y = olddir_x * sin(data->player.rot_speed) + data->player.dir_y * cos(data->player.rot_speed);
-	data->player.plane_x = data->player.plane_x * cos(data->player.rot_speed) - data->player.plane_y * sin(data->player.rot_speed);
-	data->player.plane_y = oldplane_x * sin(data->player.rot_speed) + data->player.plane_y * cos(data->player.rot_speed);
+	double	olddir_x;
+	double	oldplane_x;
+
+	olddir_x = data->player.dir_x;
+	oldplane_x = data->player.plane_x;
+	data->player.dir_x = data->player.dir_x * cos(data->player.rot_speed) \
+		- data->player.dir_y * sin(data->player.rot_speed);
+	data->player.dir_y = olddir_x * sin(data->player.rot_speed) \
+		+ data->player.dir_y * cos(data->player.rot_speed);
+	data->player.plane_x = data->player.plane_x * cos(data->player.rot_speed) \
+		- data->player.plane_y * sin(data->player.rot_speed);
+	data->player.plane_y = oldplane_x * sin(data->player.rot_speed) \
+		+ data->player.plane_y * cos(data->player.rot_speed);
 }
 
 void	keypress_left(t_data *data)
 {
-	double olddir_x = data->player.dir_x;
-	double oldplane_x = data->player.plane_x;
-	data->player.dir_x = data->player.dir_x * cos(-data->player.rot_speed) - data->player.dir_y * sin(-data->player.rot_speed);
-	data->player.dir_y = olddir_x * sin(-data->player.rot_speed) + data->player.dir_y * cos(-data->player.rot_speed);
-	data->player.plane_x = data->player.plane_x * cos(-data->player.rot_speed) - data->player.plane_y * sin(-data->player.rot_speed);
-	data->player.plane_y = oldplane_x * sin(-data->player.rot_speed) + data->player.plane_y * cos(-data->player.rot_speed);
+	double	olddir_x;
+	double	oldplane_x;
+
+	olddir_x = data->player.dir_x;
+	oldplane_x = data->player.plane_x;
+	data->player.dir_x = data->player.dir_x * cos(-data->player.rot_speed) \
+		- data->player.dir_y * sin(-data->player.rot_speed);
+	data->player.dir_y = olddir_x * sin(-data->player.rot_speed) \
+		+ data->player.dir_y * cos(-data->player.rot_speed);
+	data->player.plane_x = data->player.plane_x * cos(-data->player.rot_speed) \
+		- data->player.plane_y * sin(-data->player.rot_speed);
+	data->player.plane_y = oldplane_x * sin(-data->player.rot_speed) \
+		+ data->player.plane_y * cos(-data->player.rot_speed);
 }
 
 int	key_press(int key, t_data *data)
